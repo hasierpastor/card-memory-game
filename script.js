6641efd2894c1ad66a35a5e71c33ad5ff7ca9b9f
Hasier Pastor <hasier.pastor@gmail.com>
2019-05-15 12:27:40 -0700

    refactored code

--- script.js
+++ script.js
@@ -1,108 +1,109 @@
-var cards = document.querySelectorAll('.card');
-var flipped_cards = [];
-var lockboard = false;
-var clicks = 0;
-var score = document.getElementById('score');
-var matched_cards = [];
-var lowestScore = localStorage.getItem('newLowScore');
-var newGameButton = document.getElementById('new-game');
-var modal = document.getElementById('modal');
-var finalScore = document.getElementById('final-score');
-var currentLowScore = document.getElementById('low-score');
-var firstGameButton = document.getElementById('first-game');
-var cardContainer = document.getElementById('card-container');
-var header = document.getElementById('head');
+(function() {
+  var cards = document.querySelectorAll('.card');
+  var flipped_cards = [];
+  var lockboard = false;
+  var clicks = 0;
+  var score = document.getElementById('score');
+  var matched_cards = [];
+  var lowestScore = localStorage.getItem('newLowScore');
+  var newGameButton = document.getElementById('new-game');
+  var modal = document.getElementById('modal');
+  var finalScore = document.getElementById('final-score');
+  var currentLowScore = document.getElementById('low-score');
+  var firstGameButton = document.getElementById('first-game');
+  var cardContainer = document.getElementById('card-container');
+  var header = document.getElementById('head');
 
-//shuffle cards
-function randomize_cards(arr) {
-  for (var i = 0; i < arr.length; i++) {
-    var random_num = Math.floor(Math.random() * (arr.length - 1));
-    arr[i].style.order = random_num;
+  window.onload = function() {
+    firstGameButton.addEventListener('click', firstGame);
+    randomizeCards(cards);
+    cards.forEach(function(card) {
+      card.addEventListener('click', flip_card);
+    });
+  };
+
+  //shuffle cards
+  function randomizeCards(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      let random_num = Math.floor(Math.random() * (arr.length - 1));
+      arr[i].style.order = random_num;
+    }
   }
-}
 
-//unflip cards if they don't match
+  //unflip cards if they don't match
 
-function unflip_card() {
-  flipped_cards[0].classList.remove('is-flipped');
-  flipped_cards[1].classList.remove('is-flipped');
+  function unflipCard() {
+    flipped_cards[0].classList.remove('is-flipped');
+    flipped_cards[1].classList.remove('is-flipped');
 
-  lockboard = false;
-  flipped_cards = [];
-}
-
-//what happens if cards match or don't match
-
-function matching_cards() {
-  if (flipped_cards[0].dataset.name === flipped_cards[1].dataset.name) {
-    flipped_cards = [];
-    matched_cards.push(flipped_cards[0]);
-    matched_cards.push(flipped_cards[1]);
     lockboard = false;
-  } else {
-    setTimeout(unflip_card, 1500);
+    flipped_cards = [];
   }
-}
 
-function flip_card() {
-  if (lockboard) return;
-  if (
-    !event.currentTarget.classList.contains('is-flipped') &&
-    flipped_cards.length < 2
-  ) {
-    event.currentTarget.classList.add('is-flipped');
-    flipped_cards.push(event.currentTarget);
-    score.innerText = 'score: ' + ++clicks;
-  }
-  if (flipped_cards.length === 2) {
-    lockboard = true;
-    matching_cards();
-  }
-  endGame();
-}
+  //what happens if cards match or don't match
 
-function endGame() {
-  if (matched_cards.length === 16) {
-    if (lowestScore === null) {
-      lowestScore = Infinity;
+  function matching_cards() {
+    if (flipped_cards[0].dataset.name === flipped_cards[1].dataset.name) {
+      flipped_cards = [];
+      matched_cards.push(flipped_cards[0]);
+      matched_cards.push(flipped_cards[1]);
+      lockboard = false;
+    } else {
+      setTimeout(unflipCard, 1500);
     }
-    if (clicks < lowestScore) {
-      localStorage.setItem('newLowScore', clicks);
-      lowestScore = localStorage.getItem('newLowScore');
-    }
-    console.log(lowestScore);
-    modal.classList.add('show');
-    cardContainer.classList.add('opacity');
-    score.classList.add('opacity');
-    finalScore.innerText = 'Final score: ' + clicks;
-    currentLowScore.innerText = 'Low score: ' + lowestScore;
   }
-}
 
-function newGame() {
-  cards.forEach(function(card) {
-    card.classList.remove('is-flipped');
-    modal.classList.remove('show');
-    cardContainer.classList.remove('opacity');
-    score.classList.remove('opacity');
-    matched_cards = [];
-    clicks = 0;
-    score.innerText = 'score: ';
-  });
-}
+  function flip_card() {
+    if (lockboard) return;
+    if (
+      !event.currentTarget.classList.contains('is-flipped') &&
+      flipped_cards.length < 2
+    ) {
+      event.currentTarget.classList.add('is-flipped');
+      flipped_cards.push(event.currentTarget);
+      score.innerText = 'score: ' + ++clicks;
+    }
+    if (flipped_cards.length === 2) {
+      lockboard = true;
+      matching_cards();
+    }
+    endGame();
+  }
 
-function firstGame() {
-  cardContainer.classList.remove('hide');
-  score.classList.remove('hide');
-  header.classList.add('hide');
-}
+  function endGame() {
+    if (matched_cards.length === 16) {
+      if (lowestScore === null) {
+        lowestScore = Infinity;
+      }
+      if (clicks < lowestScore) {
+        localStorage.setItem('newLowScore', clicks);
+        lowestScore = localStorage.getItem('newLowScore');
+      }
+      modal.classList.add('show');
+      cardContainer.classList.add('opacity');
+      score.classList.add('opacity');
+      finalScore.innerText = 'Final score: ' + clicks;
+      currentLowScore.innerText = 'Low score: ' + lowestScore;
+    }
+  }
 
-window.onload = function() {
-  firstGameButton.addEventListener('click', firstGame);
-  randomize_cards(cards);
-  cards.forEach(function(card) {
-    card.addEventListener('click', flip_card);
-  });
-};
+  function newGame() {
+    cards.forEach(function(card) {
+      card.classList.remove('is-flipped');
+      modal.classList.remove('show');
+      cardContainer.classList.remove('opacity');
+      score.classList.remove('opacity');
+      matched_cards = [];
+      clicks = 0;
+      score.innerText = 'score: ';
+    });
+  }
 
-newGameButton.addEventListener('click', newGame);
+  function firstGame() {
+    cardContainer.classList.remove('hide');
+    score.classList.remove('hide');
+    header.classList.add('hide');
+  }
+
+  newGameButton.addEventListener('click', newGame);
+});
